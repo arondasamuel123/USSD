ef1c7051c9c9786a1b1aa9503728b4200d927f04
root <arondasamuel123@gmail.com>
2018-04-02 12:28:30 +0300

    session for employer

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -73,9 +73,11 @@ class USSDController extends Controller
         
         
     }
-        else {
+    elseif($user->accounttype=1) {
+
             return "Welcome"."  ".$user->name. PHP_EOL."Please choose a service".PHP_EOL."1.Mama wa Nguo".PHP_EOL."2.Electrician".PHP_EOL."3.Gas Delivery".PHP_EOL."4.Clean Water Delivery".PHP_EOL;
         }
+
         $this->sendResponse($response, 2);
     }
     public function getCityInput($input,$phoneNumber) {
@@ -215,6 +217,7 @@ class USSDController extends Controller
         $this->sendResponse($response, 2);
 
        }
+       
 
 
         protected function levelOneProcess($input,$phoneNumber)
@@ -254,7 +257,7 @@ class USSDController extends Controller
             return $response;
 
     }
-
+   
 
 
 
